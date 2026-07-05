4d5cb8226c6e3f9259bc3f6b214494f999ae6144
ezitq <bkgksnec@gmail.com>
2026-07-05 14:18:54 +0300

    Added annotations to work with config file

--- src/main/java/com/bohdan/RailwayTicketsSystemApplication.java
+++ src/main/java/com/bohdan/RailwayTicketsSystemApplication.java
@@ -1,9 +1,14 @@
 package com.bohdan;
 
+import com.bohdan.config.TariffProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(value = TariffProperties.class)
+@ConfigurationPropertiesScan
 public class RailwayTicketsSystemApplication {
 
     public static void main(String[] args) {
